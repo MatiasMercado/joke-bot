752591bc2e6ffb2786e6dbceed4432ebf0aa8dc9
MatiasMercado <mmercado@itba.edu.ar>
2019-04-04 12:03:39 +0200

    Add error handler

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const logger = require('./config/logger');
 const webhookRouter = require('./routes/webhook');
 
 const app = express();
@@ -12,7 +13,12 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use('/webhook', webhookRouter);
 
+// error handler
+app.use((err, req, res, next) => {
+  	logger.error(err);
+  	res.sendStatus(err.status || 500);
+});
+
 // Start app
 const port = process.env.PORT || 8080; 
 app.listen(port);
-
